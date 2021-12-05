77a259384a2de534a0bbc05798eb9379863597c5
Diana <diana.lyah.96@mail.ru>
2021-12-05 23:59:47 +0300

    addition grid

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
-import { AppBar, Button, Container, IconButton, Toolbar, Typography, Box } from '@material-ui/core';
+import { AppBar, Button, Container, IconButton, Toolbar, Typography, Box, Paper, Grid, Card, CardMedia, CardContent, CardActions } from '@material-ui/core';
 import MenuIcon from '@material-ui/icons/Menu';
+import LayerIcon from '@material-ui/icons/Layers';
+import PlayCircleFilledIcon from '@material-ui/icons/PlayCircleFilled';
 import { makeStyles } from '@material-ui/core/styles';
 
 
@@ -13,12 +15,36 @@ const useStyles = makeStyles((theme) => ({
   },
   title: {
     flexGrow: 1
+  },
+  mainFeaturesPost: {
+    position: 'relative',
+    color: theme.palette.common.white,
+    marginBottom: theme.spacing(4),
+
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat',
+    backgroundPosition: 'center',
+  },
+  overlay: {
+    position: 'absolute',
+    top: 0,
+    bottom: 0,
+    right: 0,
+    left: 0,
+    backgroundOverlay: 'rgba(0,0,0,.3)',
+  },
+  mainFeaturesPostContent: {
+    position: 'relative',
+    padding: theme.spacing(10),
+    marginTop: theme.spacing(10)
+
   }
 }))
+const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 function App() {
   const classes = useStyles();
   return (
-    <div className='App'>
+    <>
       <AppBar position='fixed' color='secondary'>
         <Container fixed>
           <Toolbar>
@@ -26,7 +52,7 @@ function App() {
               <MenuIcon />
             </IconButton>
             <Typography variant="h6" className={classes.title} >
-              Candy company
+              AESTHETICS
             </Typography>
             <Box mr={3} >
               <Button color='inharit' variant='outlined'>
@@ -38,9 +64,110 @@ function App() {
               </Button>
           </Toolbar>
         </Container>
-
       </AppBar>
-    </div>
+
+      <main>
+        <Paper className={classes.mainFeaturesPost}
+          style={{ backgroundImage: 'url(https://source.unsplash.com/random)' }}>
+          <Container fixed>
+            <div className={classes.overlay} />
+            <Grid container>
+              <Grid item md={6}>
+
+                <div className={classes.mainFeaturesPostContent}>
+                  <Typography
+                    component="h1"
+                    variant='h3'
+                    color='inherit'
+                    gutterBottom
+                  >
+                    Aesthetics!!!!!!!!!!!
+                  </Typography>
+
+                  <Typography
+                    variant="h5"
+                    color='inherit'
+                    paragraph
+                  >
+                    "AESTHETICS is a philosophical science that studies two interconnected circles of phenomena:
+                    the sphere of the aesthetic as a specific manifestation of the value attitude of man to the
+                    world and the sphere of artistic activity of people."
+                  </Typography>
+
+                  <Button variant="contained" color='secondary'>
+                    Learn more
+                  </Button>
+                </div>
+              </Grid>
+            </Grid>
+
+          </Container>
+        </Paper>
+        <div className={classes.mainContent}>
+          <Container maxWidth='md'>
+            <Typography
+              variant='h2' align='center' color='textPrimary' gutterBottom>
+              Sweets
+            </Typography>
+            <Typography
+              variant='h5' align='center' color='textSecondary' paragraph>
+              "AESTHETICS is a philosophical science that studies two interconnected circles of phenomena:
+              the sphere of the aesthetic as a specific manifestation of the value attitude of man to the
+              world and the sphere of artistic activity of people. "
+            </Typography>
+            <div className={classes.mainButtons}>
+              <Grid container spacing={5} justify="center">
+                <Grid item>
+                  <Button variant='contained' color='secondary'>
+                    Start now
+                  </Button>
+                </Grid>
+                <Grid item>
+                  <Button variant='outlined' color='secondary'>
+                    Learn more
+                  </Button>
+                </Grid>
+              </Grid>
+            </div>
+          </Container>
+        </div>
+        <Container className={classes.cardGrid} maxWidth='md'>
+          <Grid container spacing={4}>
+            {cards.map(card => (
+              <Grid item key={card} xs={12} sm={6} md={4}>
+                <Card className={classes.card}>
+                  <CardMedia
+                    classname={classes.CardMedia}
+                    image='https://source.unsplash.com/random'
+                    title='image title'
+                  />
+                  <CardContent className={classes.cardContent}>
+                    <Typography variant='h5' gutterBottom>
+                      Blog Post
+                    </Typography>
+                    <Typography >
+                      Blog Post!!!
+                    </Typography>
+                  </CardContent>
+                  <CardActions>
+                    <Button size="small" color='secondary'>
+                      View
+                    </Button>
+                    <Button size="small" color='secondary'>
+                      Edit
+                    </Button>
+                    <LayerIcon/>
+                    <PlayCircleFilledIcon>
+                    </PlayCircleFilledIcon>
+                  </CardActions>
+                </Card>
+              </Grid>
+            ))}
+          </Grid>
+        </Container>
+      </main>
+    </>
+
   );
 }
 
